29dba0b2056c769264aebcc8eb8290cf68486da9
YubiYubi719 <littlebe0719@gmail.com>
2024-07-12 20:01:31 +0800

    update makefile

--- Q2/Q2.cpp
+++ Q2/Q2.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
-// *vvvvvvvvvvvInclude necessary libraries here!!vvvvvvvvvvv*
-
-
-// *^^^^^^^^^^^Include necessary libraries here!!^^^^^^^^^^^*
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+bool myCmp(pair<int,int> &a, pair<int,int> &b){
+    return a.first*b.second < a.second*b.first;
+}
+
 int main(int argc, char* argv[]){
-    // *vvvvvvvvvvvFinish your code here!!vvvvvvvvvvv*
+    ifstream input(argv[1]);
+    int n;
+    int sum = 0;
+    vector<pair<int,int>> objs;
+    int total_cost = 0;
 
+    input >> n;
+    objs.resize(n);
+    for(int i = 0; i < n; i++){
+        input >> objs[i].first;
+    }
+    for(int i = 0; i < n; i++){
+        input >> objs[i].second;
+    }
 
-    // *^^^^^^^^^^^Finish your code here!!^^^^^^^^^^^*
+    sort(objs.begin(),objs.end(),myCmp);
+
+    for(int i = 0; i < n-1; i++){
+        sum += objs[i].first;
+        total_cost += sum * objs[i+1].second;
+    }
+
+    ofstream output(argv[2]);
+    output << total_cost << '\n';
+    output.close();
+
     return 0;
 }
+
+// #include <iostream>
+// // *vvvvvvvvvvvInclude necessary libraries here!!vvvvvvvvvvv*
+
+
+// // *^^^^^^^^^^^Include necessary libraries here!!^^^^^^^^^^^*
+// using namespace std;
+
+// int main(int argc, char* argv[]){
+//     // *vvvvvvvvvvvFinish your code here!!vvvvvvvvvvv*
+
+
+//     // *^^^^^^^^^^^Finish your code here!!^^^^^^^^^^^*
+//     return 0;
+// }
