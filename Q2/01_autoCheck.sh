a5a12ec7c354f0d60668516af01a6b06b66bdd42
YubiYubi719 <littlebe0719@gmail.com>
2024-07-12 20:13:29 +0800

    update ./01

--- Q2/01_autoCheck.sh
+++ Q2/01_autoCheck.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 PATNUM=1000
 
+make > make.log
+
 if [ -d "outputResult" ]; then
-    rm outputResult/*
-    rmdir outputResult
+    rm -rf outputResult
 fi
 if [ -d "checkResult" ]; then
-    rm checkResult/*
-    rmdir checkResult
+    rm -rf checkResult
 fi
 mkdir outputResult
 mkdir checkResult
@@ -20,5 +20,9 @@ for((i = 1; i <= ${PATNUM}; i = i+1)) do
         echo -e "\033[0;34mPass  \033[m  \033[0;32mPattern No.$i "
     else
         echo -e "\033[31m--- Check Fail ---\033[0m"
+        rm -f make.log
+        exit
     fi
 done
+
+rm -f make.log
--- Q2/Q2.cpp
+++ Q2/Q2.cpp
@@ -1,54 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
+// *vvvvvvvvvvvInclude necessary libraries here!!vvvvvvvvvvv*
+
+
+// *^^^^^^^^^^^Include necessary libraries here!!^^^^^^^^^^^*
 using namespace std;
 
-bool myCmp(pair<int,int> &a, pair<int,int> &b){
-    return a.first*b.second < a.second*b.first;
-}
-
 int main(int argc, char* argv[]){
-    ifstream input(argv[1]);
-    int n;
-    int sum = 0;
-    vector<pair<int,int>> objs;
-    int total_cost = 0;
+    // *vvvvvvvvvvvFinish your code here!!vvvvvvvvvvv*
 
-    input >> n;
-    objs.resize(n);
-    for(int i = 0; i < n; i++){
-        input >> objs[i].first;
-    }
-    for(int i = 0; i < n; i++){
-        input >> objs[i].second;
-    }
 
-    sort(objs.begin(),objs.end(),myCmp);
-
-    for(int i = 0; i < n-1; i++){
-        sum += objs[i].first;
-        total_cost += sum * objs[i+1].second;
-    }
-
-    ofstream output(argv[2]);
-    output << total_cost << '\n';
-    output.close();
-
+    // *^^^^^^^^^^^Finish your code here!!^^^^^^^^^^^*
     return 0;
 }
-
-// #include <iostream>
-// // *vvvvvvvvvvvInclude necessary libraries here!!vvvvvvvvvvv*
-
-
-// // *^^^^^^^^^^^Include necessary libraries here!!^^^^^^^^^^^*
-// using namespace std;
-
-// int main(int argc, char* argv[]){
-//     // *vvvvvvvvvvvFinish your code here!!vvvvvvvvvvv*
-
-
-//     // *^^^^^^^^^^^Finish your code here!!^^^^^^^^^^^*
-//     return 0;
-// }
